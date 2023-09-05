539b786fdc4f8dcaabf7fdbc7f21bc08c3e60435
Bob Witmer <bwitmer@golden.net>
2023-09-05 11:44:13 -0400

    Initial Commit

---
+++ GuacChain/GuacChainApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuacChainApp.swift
+//  GuacChain
+//
+//  Created by Bob Witmer on 2023-09-05.
+//
+
+import SwiftUI
+
+@main
+struct GuacChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
